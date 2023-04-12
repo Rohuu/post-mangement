ae45cf56f2e291dab5833116493d741987a56c20
Rohuu <rohuu99022@gmail.com>
2023-04-12 23:11:47 +0530

    creating postController

---
+++ src/main/java/com/rohit/controller/postController.java
@@ -0,0 +1,23 @@
+package com.rohit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class postController {
+
+    @RequestMapping(method = RequestMethod.GET,value = "/createPost")
+    public String createPost(){
+        System.out.println("inside createPost method");
+        return "createPost";
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "/home")
+    public String home(){
+        System.out.println("inside home method");
+        return "home";
+    }
+}
